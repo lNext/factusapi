d2d93e4512a927d1894d670405f419b97f4fa607
Sebastián <sebastian.contactoweb@gmail.com>
2025-01-03 09:43:01 -0500

    Seguridad

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-import express from 'express';
-import factusRoutes from './routes/factusRoutes.js';
+import express from "express";
+import factusRoutes from "./routes/factusRoutes.js";
+
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api', factusRoutes);
+app.use("/api", factusRoutes);
 
 app.use((req, res, next) => {
   res.status(404).json({
-    message: 'Ruta no encontrada',
+    message: "Ruta no encontrada",
   });
 });
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({
-    message: 'Ocurrió un error en el servidor',
+    message: "Ocurrió un error en el servidor",
   });
 });
 
-
-app.listen(3007, () => {
-  console.log(`Servidor corriendo en el puerto 3007`);
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
 });
 
 export default app;
